a14e2ea175e4752b5e7ba3eca4abf9a973b83e80
nawiq7208 <ipsurbakti7208@gmail.com>
2022-05-27 17:57:17 +0800

    Update UserList.js

--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -14,6 +14,15 @@ const getUsers = async () => {
   setUser(response.data);
 }
 
+const deleteUser = async (id) => {
+  try {
+      await axios.delete(`http://localhost:5000/users/${id}`)
+      getUsers();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
   return (
     <div className="columns mt-5 is-centered">
         <div className="column is-half">
@@ -37,7 +46,7 @@ const getUsers = async () => {
                   <td>{users.gender}</td>
                   <td>
                     <Link to={`edit/${users.id}`} className="button is-small is-info">Edit</Link>
-                    <button className="button is-small is-danger">Delete</button>
+                    <button onClick={()=> deleteUser(users.id)} className="button is-small is-danger">Delete</button>
                   </td>
                 </tr>
                 ))}
