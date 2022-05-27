f37e31089cb108bb2f7b0deafc9dc2e7bc97f871
nawiq7208 <ipsurbakti7208@gmail.com>
2022-05-27 15:28:28 +0800

    update fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import UserList from "../components/UserList";
+import UserList from "./components/UserList";
 
 function App() {
   return (
--- frontend/src/components/UserList.js
+++ frontend/src/components/UserList.js
@@ -1,10 +1,41 @@
-import React from 'react'
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 
 const UserList = () => {
+const [users, setUser] = useState([]);
+
+useEffect(()=>{
+  getUsers();
+}, []);
+
+const getUsers = async () => {
+  const response = await axios.get('http://localhost:5000/users');
+  console.log(response.data);
+}
+
   return (
-    <div className="columns mt-5">
+    <div className="columns mt-5 is-centered">
         <div className="column is-half">
-            <h1>Lorem ipsum dolor sit amet.</h1>
+            <table className='table is-striped is-fullwidth'>
+              <thead>
+                <tr>
+                  <th>No</th>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Gender</th>
+                  <th>Actions</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                  <td></td>
+                </tr>
+              </tbody>
+            </table>
         </div>
     </div>
   )
