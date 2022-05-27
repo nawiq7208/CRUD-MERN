311fd3dfa6759546967eeb22545536e56eb85d71
nawiq7208 <ipsurbakti7208@gmail.com>
2022-05-27 16:44:25 +0800

    update fe

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,13 @@
 import {BrowserRouter, Routes, Route} from "react-router-dom";
 import AddUser from "./components/AddUser";
 import UserList from "./components/UserList";
-import AddUser from "./components/AddUser";
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
       <Route path="/" element={<UserList/>}/>
-      <Route path="add" element={<AddUser/>}/>
+      <Route path="/add" element={<AddUser/>}/>
     </Routes>
     </BrowserRouter>
   );
